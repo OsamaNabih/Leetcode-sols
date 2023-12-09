55679f7b1f041d5e01d8ebcb0625a4c8f43b77fb
Osama Nabih <Nabih_The_4th@yahoo.com>
2023-12-09 17:39:36 +0200

    Time: 24 ms (45.44%), Space: 25.3 MB (83.33%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
+
+    void swap(vector<int>& arr, int idx1, int idx2) {
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+    }
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+        int count = nums.size();
+        int itr = 0;
+        int origIdx = 0;
+        int currIdx = 0;
+        int rotation = k % nums.size();
+        int newIdx;
+        while(itr < count) {
+            newIdx = (origIdx + rotation) % nums.size();
+            if (newIdx == currIdx) {
+                currIdx = (currIdx + 1) % nums.size();
+                origIdx = currIdx;
+                itr++;
+                continue;
+            }
+            swap(nums, currIdx, newIdx);
+            origIdx = newIdx;
+            itr++;
+        }
     }
 };
