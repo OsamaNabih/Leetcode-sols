8a5478d407778c9d96fb3f3bca9895713f31c67c
Osama Nabih <Nabih_The_4th@yahoo.com>
2023-12-18 19:12:40 +0200

    Time: 1914 ms (5.03%), Space: 50 MB (14.55%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[10010];
+    bool solve(vector<int>& nums, int idx) {        
+        bool ans = false;
+        if (dp[idx] != -1) return dp[idx];
+        for(int i = 1; i <= nums[idx]; i++) {
+            if (idx + i >= nums.size()) {
+                break;
+            } else if (idx + i == nums.size() - 1) {
+                ans = true;
+                break;
+            }
+            ans = ans || solve(nums, idx + i);
+        }
+        dp[idx] = ans ? 1 : 0;
+        return ans;
+    }
+    bool canJump(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        return solve(nums, 0) || nums.size() == 1;
+    }
+};
