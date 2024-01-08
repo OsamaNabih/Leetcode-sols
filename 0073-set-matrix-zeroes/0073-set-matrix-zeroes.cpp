1cf24b54b35f9a9f7450f8254e8e3c76b2c640fe
Osama Nabih <Nabih_The_4th@yahoo.com>
2024-01-08 08:31:34 +0200

    Time: 14 ms (32.93%), Space: 13.9 MB (52.81%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,24 +3,46 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int rows = matrix.size();
         int cols = matrix[0].size();
-        vector<bool> zeroRows(rows, false);
-        vector<bool> zeroCols(cols, false);
+        
+        
+        bool colZero = false;
+        bool rowZero = false;
+        for(int j = 0; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                rowZero = true;
+                break;
+            }
+        }
+        for(int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                colZero = true;
+                break;
+            }
+        }
         for(int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    zeroRows[i] = true;
-                    zeroCols[j] = true;
+                if (matrix[i][j] == 0 && i != 0 && j != 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    cout << i << " " << j << endl;
                 }
-                if (zeroRows[i]) matrix[i][j] = 0;
-                if (zeroCols[j]) matrix[i][j] = 0;
             }
         }
-         for(int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (zeroRows[i]) matrix[i][j] = 0;
-                if (zeroCols[j]) matrix[i][j] = 0;
+        cout << endl;
+        for(int i = rows - 1; i >= 1; i--) {
+            for (int j = cols - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
+        if (colZero) {
+            for (int i = 0; i < rows; i++)
+                matrix[i][0] = 0;
+        }
+        if (rowZero) {
+            for (int j = 0; j < cols; j++)
+                matrix[0][j] = 0;
+        }
+        
 
     }
 };
