4ea49f9d46dd2f95764c58854bea4340bf3440f0
Osama Nabih <Nabih_The_4th@yahoo.com>
2024-04-27 03:12:17 +0300

    Time: 4 ms (54.98%), Space: 11.9 MB (5.92%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -17,16 +17,13 @@ public:
         if (ptr == nullptr) return nullptr;
         N++;
         curr++;
-        cout << ptr->val << " " << oldRoot->val << endl;
         if (ptr->next != nullptr) {
             ListNode* nextNode = solve(ptr->next, oldRoot);
             oldRoot = nextNode;
         }
-        cout << ptr->val << " counts: " << curr << " " << N << endl;
         if (curr == ((N+1) / 2)) {
             return oldRoot->next;
         }
-        cout << "Swapping " << ptr->val << " " << oldRoot->val;
         int temp = ptr->val;
         ptr->val = oldRoot->val;
         oldRoot->val = temp;
@@ -34,7 +31,6 @@ public:
         return oldRoot->next;
     }
     ListNode* reverseList(ListNode* head) {
-        ListNode* temp = head;
         solve(head, head);
         return head;
     }
