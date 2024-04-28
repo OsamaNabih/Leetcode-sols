930d9c5296b29b42310dcff5531da06709f609fc
Osama Nabih <Nabih_The_4th@yahoo.com>
2024-04-28 22:09:28 +0300

    Time: 0 ms (100.00%), Space: 9.8 MB (33.20%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -9,14 +9,14 @@ public:
         while (i <= R) {
             if (nums[i] == 0) {
                 swap(nums[L], nums[i]);
+                L++;
+                i++;
             } else if (nums[i] == 2) {
                 swap(nums[R], nums[i]);
+                R--;
             } else {
                 i++;
             }
-            while(L < N && nums[L] == 0) L++;
-            while(R >= 0 && nums[R] == 2) R--;
-            while(i < L) i++;
         }
     }
 };
