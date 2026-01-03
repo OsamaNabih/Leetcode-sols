daec46a3882653c0187fdec1752a418d3b5b4cd4
Osama Nabih <Nabih_The_4th@yahoo.com>
2026-01-03 14:10:06 +0200

    1527-NUMBER-OF-WAYS-TO-PAINT-N-3-GRID Stats: Time: 247 ms (17.67%), Space: 45.6 MB (11.67%) - LeetHub

---
+++ 1527-number-of-ways-to-paint-n-3-grid/1527-number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int dp[5001][13];
+    int mod = 1e9 + 7;
+    unordered_map<int, vector<int>> allowed {
+        {0, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}},
+        {1, {5, 6, 8, 9, 10}},
+        {2, {5, 7, 8, 9}},
+        {3, {5, 6, 9, 10, 12}},
+        {4, {6, 10, 11, 12}},
+        {5, {1, 2, 3, 11, 12}},
+        {6, {1, 3, 4, 11}},
+        {7, {2, 9, 10, 12}},
+        {8, {1, 2, 10, 11, 12}},
+        {9, {1, 2, 3, 7}},
+        {10, {1, 3, 4, 7, 8}},
+        {11, {4, 5, 6, 8}},
+        {12, {3, 4, 5, 7, 8}},
+    };
+
+
+    unordered_map<int, string> patterns {
+        {0, "XXX"},
+        {1, "RYR"},
+        {2, "RYG"},
+        {3, "RGR"},
+        {4, "RGY"},
+        {5, "YRY"},
+        {6, "YRG"},
+        {7, "YGR"},
+        {8, "YGY"},
+        {9, "GRY"},
+        {10, "GRG"},
+        {11, "GYR"},
+        {12, "GYG"},
+    };
+
+    int solve(int n, int prev) {
+        //cout << "n: " << n <<", prev: " << prev << ": " << patterns[prev] << endl;
+        //cout << patterns[prev] << endl;
+        if (n == 0) {
+            //cout << endl;
+            return 1;
+        }
+        if (dp[n][prev] != -1) return dp[n][prev];
+        auto vars = allowed[prev];
+        //cout << vars.size() << endl;
+        int ans = 0;
+        for(int i = 0; i < vars.size(); i++) {
+            ans = (ans + 1LL*solve(n - 1, vars[i])) % mod;
+        }
+        //cout << "returning: " << ans << endl;
+        return dp[n][prev] = ans;
+    }
+    int numOfWays(int n) {
+        memset(dp, -1, sizeof(dp));
+        dp[1][0] = 12;
+        for (auto it  = allowed.begin(); it != allowed.end(); it++) {
+            if (it->first == 0) continue;
+            dp[1][it->first] = it->second.size();
+        }
+        int ans = 0;
+        for(int i = 1; i <= 12; i++) {
+            ans = (ans + (1LL*solve(n - 1, i))) % mod;
+        }
+        return ans;
+    }
+};
--- README.md
+++ README.md
@@ -85,6 +85,7 @@ Collection of LeetCode questions to ace the coding interview! - Created using [L
 |  |
 | ------- |
 | [1000-delete-columns-to-make-sorted-iii](https://github.com/OsamaNabih/Leetcode-sols/tree/master/1000-delete-columns-to-make-sorted-iii) |
+| [1527-number-of-ways-to-paint-n-3-grid](https://github.com/OsamaNabih/Leetcode-sols/tree/master/1527-number-of-ways-to-paint-n-3-grid) |
 | [2164-two-best-non-overlapping-events](https://github.com/OsamaNabih/Leetcode-sols/tree/master/2164-two-best-non-overlapping-events) |
 | [2233-number-of-smooth-descent-periods-of-a-stock](https://github.com/OsamaNabih/Leetcode-sols/tree/master/2233-number-of-smooth-descent-periods-of-a-stock) |
 | [2251-number-of-ways-to-divide-a-long-corridor](https://github.com/OsamaNabih/Leetcode-sols/tree/master/2251-number-of-ways-to-divide-a-long-corridor) |
